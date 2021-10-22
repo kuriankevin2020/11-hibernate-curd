3685f2a061bc11d508e9f014409f8fb47662c0e7
Kurian Kevin <kuriankevin2020@gmail.com>
2021-10-22 21:15:39 +0530

    SQL Command Update

--- sql/student-commands.sql
+++ sql/student-commands.sql
@@ -6,3 +6,11 @@ TRUNCATE hb_student_tracker.student;
 
 /* provide the starting index for primary key */
 ALTER TABLE hb_student_tracker.student AUTO_INCREMENT=10001;
+
+/* delete linked table data */
+ SET FOREIGN_KEY_CHECKS = 0;
+ TRUNCATE `hb-01-one-to-one-uni`.instructor_detail;
+ SET FOREIGN_KEY_CHECKS = 1;
+ 
+ /* show table structure */
+  DESC `hb-01-one-to-one-uni`.instructor_detail;
