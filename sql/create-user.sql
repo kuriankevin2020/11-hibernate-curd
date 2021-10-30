9bd937112d39a6a598ecb13ddcaab0ed654fa968
Kurian Kevin <kuriankevin2020@gmail.com>
2021-10-30 11:45:41 +0530

    Sql Script Update

--- sql/create-user.sql
+++ sql/create-user.sql
@@ -1,5 +1,5 @@
+DROP USER 'hbstudent'@'localhost';
+FLUSH privileges;
 CREATE USER 'hbstudent'@'localhost' IDENTIFIED BY 'hbstudent';
-
 GRANT ALL PRIVILEGES ON * . * TO 'hbstudent'@'localhost';
-
 ALTER USER 'hbstudent'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbstudent';
